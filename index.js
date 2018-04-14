cf0be98e05f2400dfc5c332162ed0c5996c3ad1a
Romain Bontemps <romain@ponteilla.net>
2018-04-14 17:37:58 +0200

    feat(help): Improve help

--- index.js
+++ index.js
@@ -186,9 +186,19 @@ bot.on('message', async (message) => {
     const command = args.shift().toLowerCase();
     // Commande help
     if (command === 'help') {
-      m = 'Commandes utilisateurs : !ping !time';
+      m = `**Commandes utilisateurs** :
+**!ping** : Afficher la latence du serveur
+**!time** : Donne l'heure (au cas où vous n'auriez pas de montre...)
+**!rune** _rune_ : Affiche des informations sur la rune _rune_ (ex: !rune ahstayi)
+**!petskill** _petskill_ : Affiche des informations sur let skill pet _petskill_ (!petskill shield of tardy)
+**!ps** : Alias pour !petskill
+**!pskill** : Alias pour !petskill
+`;
       if (member.roles.find('name', 'Modo') || member.roles.find('name', 'Botty_devs')) {
-        m += '\nCommandes admin : !announce';
+        m += `
+**Commandes admin** :
+**!announce** : Affiche la liste des différentes annonces et dans quel channel elles se déclenchent
+`;
       }
       message.author.send(m);
     }
